383cd945cf2386ca9f29d79c0bf0055d2a1f949a
dQNNNY <dsadenov703@gmail.com>
2025-02-01 00:04:31 +0500

    lab1

---
+++ lab1/Variables.py
@@ -0,0 +1,17 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
